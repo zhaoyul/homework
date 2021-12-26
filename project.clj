262054da96fd8f9438757c881cb81a9fceab5dc1
kevin li <kli@kli.com>
2021-12-26 18:11:39 +0800

    goon

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject homework1 "homework"
   :description "实现一个TCP通讯的小游戏"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [aleph "0.4.6"]]
+                 [aleph "0.4.6"]
+                 [gloss "0.2.6"]]
   :repl-options {:init-ns homework1.core})
--- src/homework1/core.clj
+++ src/homework1/core.clj
@@ -1,5 +1,7 @@
 (ns homework1.core
   (:require [aleph.tcp :as tcp]
+            [gloss.core :as g]
+            [gloss.io :as gio]
             [manifold.stream :as s]))
 
 (defn foo
@@ -7,23 +9,54 @@
   [x]
   (println x "Hello, World!"))
 
-(comment
 
-  (def server (tcp/start-server (fn [server-stream info]
-                                  (s/consume
-                                   #(do
-                                      (println "收到数据:" %)
-                                      (s/put! server-stream "收到消息了"))
-                                   server-stream))
-                                {:port 3333}))
+(defn wrap-duplex-stream
+  "s is the duplex stream: both client & server"
+  [protocol s]
+  (let [out (s/stream)] ;; device-sensor.socket.??
+    (s/connect
+     (s/map #(gio/encode protocol %) out) ;; source
+     s)                                   ;; sink
+    (s/splice                             ;; put! --> sink --- source -> take!
+     out                                  ;; sink
+     (gio/decode-stream s protocol))))
+
+
+
+(comment
+  ;; [[xpos ypos] color]  1:black 2:white
+  (g/defcodec msg-codec [[:byte :byte] :byte])
+
+  (defn broadcast [all-streams msg]
+    (run! (fn [s] (s/try-put! s msg 1000))
+          all-streams))
+
+  (def all-server-streams (atom #{}))
+  (def server (tcp/start-server
+               (fn [server-stream info]
+                 (swap! all-server-streams conj server-stream)
+                 (s/consume
+                  #(do
+                     (println "收到数据:" %)
+                     (broadcast @all-server-streams %))
+                  server-stream))
+               {:port 3333}))
+
+
   (.close server)
 
-  (def client (tcp/client {:host "localhost"
-                           :port 3333}))
+  (def client1
+    (wrap-duplex-stream
+     msg-codec
+     @(tcp/client {:host "localhost"
+                   :port 3333})))
 
 
-  (type @client)
+  (s/consume println client1)
 
-  (s/put! @client "hello world")
+  (s/put! client1 [[3 4] 1])
+
+  
+
 
   )
